cd6ba96f87664aba3b04fc81b2f4b5af5ffc4103
Hossain Mohammad Maruf <47243571+marufhossain012@users.noreply.github.com>
2023-06-10 02:22:20 +0600

    Add files via upload

---
+++ Credit.java
@@ -0,0 +1,24 @@
+package com.mycompany.pharmacy_management_system;
+
+/*
+<< Pharmacy Management System Project>>
+Date of Presentation : 16 June 2023
+Group : I   
+Name of Group members : Md. Emran Hossain (Id-2221132642) Hossain Mohammad Maruf (Id-1712604042) Ariful Anam Afridi(Id-2222859042)
+Instructor : ASM Sabiqul Hassan
+Section : CSE215L.16
+*/
+
+// credit class
+class Credit
+{
+    static String p1 = "Hossain Mohammad Maruf ", p2 = "Ariful Anam Afridi" , p3 ="Md. Emran Hossain";
+
+    static void printNames()
+    {
+        System.out.println("\n\nBrothers Pharmacy Management System Developing Credit Goes To: ");
+        System.out.println(  p1);
+        System.out.println(  p2);
+        System.out.println(  p3);
+    }
+}
---
+++ Pair.java
@@ -0,0 +1,23 @@
+package com.mycompany.pharmacy_management_system;
+
+/*
+<< Pharmacy Management System Project>>
+Date of Presentation : 16 June 2023
+Group : I   
+Name of Group members : Md. Emran Hossain (Id-2221132642) Hossain Mohammad Maruf (Id-1712604042) Ariful Anam Afridi(Id-2222859042)
+Instructor : ASM Sabiqul Hassan
+Section : CSE215L.16
+*/
+
+// pair class, needed for the arraylist of medicine name and price
+class Pair
+{
+    String value1;
+    String value2;
+
+    Pair(String a, String b)
+    {
+        value1 = a;
+        value2 = b;
+    }
+}
